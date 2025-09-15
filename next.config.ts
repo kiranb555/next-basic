0e6766b6d8738648de35615894c5ab4c60775071
kiranb555 <kiran.basavaraj05@gmail.com>
2025-09-16 00:32:48 +0530

    Added our page as default instead of Readme.md

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? '/next-basic' : '',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
